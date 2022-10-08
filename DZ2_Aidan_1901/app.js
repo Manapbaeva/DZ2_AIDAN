22be8975c49d006a7d7498310f21a3f2d7f5d9fb
Manapbaeva <115273434+Manapbaeva@users.noreply.github.com>
2022-10-08 17:50:06 +0600

    Add files via upload

---
+++ DZ2_Aidan_1901/app.js
@@ -0,0 +1,17 @@
+//Задача №1
+//1) while
+let i = i;
+while (i <= 50) {
+    document.write(i + '<br>');
+    i++;
+}
+
+//2)for
+for (let i = 1; i <= 50; i++) {
+    document.write(i + '<br>');
+}
+
+//Задача №2
+let arr = ['Привет, ', 'Мир', '!'];
+alert(arr[0] + arr[1] + arr[2]);
+console.log(arr)
